fdb013d849f13fbfb670d1c012b1e90e4461352d
HugoChen201 <145307533+HugoChen201@users.noreply.github.com>
2023-12-10 17:43:04 -0500

    Update LyricsDataCleaning.py
    Update

--- LyricsDataCleaning.py
+++ LyricsDataCleaning.py
@@ -32,21 +32,20 @@ print(cleaned_song.head(10000))
 
 
 #Used for content-based filtering
-filt.to_csv('top_30_recs.csv', index=False)
-#playlist_data = pd.read_csv('AMS325SpotifyPlaylist.csv')
+#Use the filt dataset of 30 recommended songs based off of audio metrics.
 playlist_songs = filt[['track_name', 'track_artist']].drop_duplicates()
 
-# Keep track of selected songs
+#Used to keep track of selected songs.
 selected_songs_set = set()
 selected_artists_set = set()
 recommendations = []
 
-# TF-IDF for lyrics
+#TF-IDF for lyric frequency analysis
 tfidf = TfidfVectorizer(analyzer='word', stop_words='english', max_features=500)
 lyrics_matrix = tfidf.fit_transform(cleaned_song['lyrics'])
 print(lyrics_matrix)
 
-# Content-based filtering using cosine similarity
+#Content-based filtering using cosine similarity
 cosine_similarities = cosine_similarity(lyrics_matrix)
 similarities = {}
 
@@ -59,22 +58,32 @@ for i in range(len(cosine_similarities)):
 
 
 class ContentBasedRecommender:
+    
     def __init__(self, matrix):
         self.matrix_similar = matrix
 
     def recommend(self, song, artist, number_of_songs):
-        # Check if the key exists in the matrix_similar dictionary
+        """Creates recommendations based off of the cosine similiarity score.
+        Args:
+        song(str): song name
+        artist(str): artist name
+        number_of_songs(int): number of songs to recommend
+        
+        returns(df): dataframe including original song, original artist, similarity score, recommended song, recommended artist
+        """
+        #Check if the key exists in the matrix_similar dictionary
         if song in self.matrix_similar:
-            # Retrieve recommended songs excluding the original song
+            #Retrieve recommended songs excluding the original song
             recommended_songs = [
                 (similarity, rec_song, rec_artist)
                 for similarity, rec_song, rec_artist in self.matrix_similar[song]
                 if rec_song != song and similarity < 1.0  # Exclude songs with similarity 1.0
             ][:number_of_songs]
 
-            # Prints the recommended songs for each of the songs we are randomly choosing.
+            #Prints the recommended songs for each of the songs we are randomly choosing.
             rec_items = len(recommended_songs)
             print(f'The {rec_items} recommended songs for {song} are:')
+            #Prints out 
             for i in range(rec_items):
                 print(f"Song {i + 1}:")
                 print(
@@ -90,7 +99,8 @@ class ContentBasedRecommender:
         else:
             print(f"No recommendations available for {song}")
             
-# Creates the recommendation using the ContentBasedRecommender function using the cosine similarity metric.
+#Creates the recommendation using the ContentBasedRecommender function using the cosine similarity metric.
+#Recommends a song for every row in filt(dataset of 30 recommended songs based off of audio metrics)
 for _, row in filt.iterrows():
     recommendation = {
         'track_name': row['track_name'],
@@ -106,25 +116,23 @@ for _, row in filt.iterrows():
     )
     recommendations.append(recommendations_df)
 
-# Concatenate all recommendations into a single DataFrame
+#Concatenate all given recommendations into a single DataFrame
 all_recommendations_df = pd.concat(recommendations, ignore_index=True)
 
-# Sort the recommendation by similarity score
+#Sort the recommendation by similarity score
 all_recommendations_df.sort_values(by='Similarity', ascending=False, inplace=True)
-
 all_recommendations_df.to_csv('recommendations.csv', index=False)
 
-# Display the recommendations
+#Display the recommendations
 print(all_recommendations_df.head(30))
 
-# A plot for similarity score
+#Plot based off of similiarity score and recommended song
 plt.figure(figsize=(10, 6))
-plt.bar(all_recommendations_df['Recommended Song'], all_recommendations_df['Similarity'], color='blue')
+top_15_recommendations_df = all_recommendations_df.head(15)
+plt.bar(top_15_recommendations_df['Recommended Song'], top_15_recommendations_df['Similarity'], color='blue')
 plt.xlabel('Recommended Song')
 plt.ylabel('Similarity Score')
-plt.title('Top Recommended Songs and Their Similarity Scores')
+plt.title('Top 15 Recommended Songs and Their Similarity Scores')
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
-
-# Display the plot
 plt.show()
