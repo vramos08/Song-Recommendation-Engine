d1a15f0a1be3d2c4c597c4e707b9fa768cf7a564
HugoChen201 <145307533+HugoChen201@users.noreply.github.com>
2023-12-10 17:14:50 -0500

    Update LyricsDataCleaning.py
    Updated recommendation

--- LyricsDataCleaning.py
+++ LyricsDataCleaning.py
@@ -31,77 +31,71 @@ cleaned_song = cleaned_song.reset_index(drop=True)
 print(cleaned_song.head(10000))
 
 
-
 #Used for content-based filtering
-
+filt.to_csv('top_30_recs.csv', index=False)
 #playlist_data = pd.read_csv('AMS325SpotifyPlaylist.csv')
-playlist_songs = top_30_recs[['track_name', 'track_artist']].drop_duplicates()
+playlist_songs = filt[['track_name', 'track_artist']].drop_duplicates()
 
-selected_songs = []
-#Number of songs we want to get from the original playlist.
-target_songs = 10
-
-# Keep selecting random songs until we get 10 songs that are also in cleaned_song
-while len(selected_songs) < target_songs:
-    # Randomly select a song from playlist_data
-    random_song = playlist_songs.sample().iloc[0]
-    
-    # Check if the selected song is in cleaned_song
-    if (cleaned_song['track_name'] == random_song['track_name']).any():
-        selected_songs.append(random_song)
-
+# Keep track of selected songs
+selected_songs_set = set()
+selected_artists_set = set()
 recommendations = []
 
-#TF-IDF is used to calculate the originality of a word by comparing the number of times the word appears in a document
-#with the number of documents the word appears in. This means that we are calculating the frequency of the word. 
-
-tfidf = TfidfVectorizer(analyzer='word', stop_words='english', max_features = 500)
+# TF-IDF for lyrics
+tfidf = TfidfVectorizer(analyzer='word', stop_words='english', max_features=500)
 lyrics_matrix = tfidf.fit_transform(cleaned_song['lyrics'])
 print(lyrics_matrix)
 
-#Content-based filtering uses cosine similarity for recommendation
-cosine_similarities = cosine_similarity(lyrics_matrix) 
-#Similar words will be stored in a dictionary which we will then use to recommend songs.
+# Content-based filtering using cosine similarity
+cosine_similarities = cosine_similarity(lyrics_matrix)
 similarities = {}
 
 for i in range(len(cosine_similarities)):
     similar_index = cosine_similarities[i].argsort()[:-50:-1]
     similarities[cleaned_song['track_name'].iloc[i]] = [
-        (cosine_similarities[i][x], cleaned_song['track_name'].iloc[x], cleaned_song['track_artist'].iloc[x]) for x in similar_index][1:]
-    
-#Function class to recommend a song based off cosine similarity
+        (cosine_similarities[i][x], cleaned_song['track_name'].iloc[x], cleaned_song['track_artist'].iloc[x])
+        for x in similar_index
+    ][1:]
+
+
 class ContentBasedRecommender:
     def __init__(self, matrix):
         self.matrix_similar = matrix
 
     def recommend(self, song, artist, number_of_songs):
-        # Retrieve recommended songs excluding the original song (Meaning the original song cannot be part of the recommendation)
-        recommended_songs = [
-            (similarity, rec_song, rec_artist)
-            for similarity, rec_song, rec_artist in self.matrix_similar[song]
-            if rec_song != song
-        ][:number_of_songs]
+        # Check if the key exists in the matrix_similar dictionary
+        if song in self.matrix_similar:
+            # Retrieve recommended songs excluding the original song
+            recommended_songs = [
+                (similarity, rec_song, rec_artist)
+                for similarity, rec_song, rec_artist in self.matrix_similar[song]
+                if rec_song != song and similarity < 1.0  # Exclude songs with similarity 1.0
+            ][:number_of_songs]
 
-        # Prints the recommended songs for each of the songs we are randomly choosing.
-        rec_items = len(recommended_songs)
-        print(f'The {rec_items} recommended songs for {song} are:')
-        for i in range(rec_items):
-            print(f"Song {i + 1}:")
-            print(f"{recommended_songs[i][1]} by {recommended_songs[i][2]} with {round(recommended_songs[i][0], 3)} similarity score")
+            # Prints the recommended songs for each of the songs we are randomly choosing.
+            rec_items = len(recommended_songs)
+            print(f'The {rec_items} recommended songs for {song} are:')
+            for i in range(rec_items):
+                print(f"Song {i + 1}:")
+                print(
+                    f"{recommended_songs[i][1]} by {recommended_songs[i][2]} with {round(recommended_songs[i][0], 3)} similarity score"
+                )
 
-        columns = ['Similarity', 'Recommended Song', 'Recommended Artist']
-        df = pd.DataFrame(recommended_songs, columns=columns)
-        df.insert(0, 'Original Song', song)
-        df.insert(1, 'Original Artist', artist)
+            columns = ['Similarity', 'Recommended Song', 'Recommended Artist']
+            df = pd.DataFrame(recommended_songs, columns=columns)
+            df.insert(0, 'Original Song', song)
+            df.insert(1, 'Original Artist', artist)
 
-        return df
-
-#Creates the recommendation using the ContentBasedRecommender function using the cosine similarity metric.
-for song in selected_songs:
+            return df
+        else:
+            print(f"No recommendations available for {song}")
+            
+# Creates the recommendation using the ContentBasedRecommender function using the cosine similarity metric.
+for _, row in filt.iterrows():
     recommendation = {
-        'track_name': song['track_name'],
-        'track_artist': song['track_artist'],
-        'number_of_songs': 10
+        'track_name': row['track_name'],
+        'track_artist': row['track_artist'],
+        'number_of_songs': 1
     }
 
     recommender = ContentBasedRecommender(similarities)
@@ -112,24 +106,23 @@ for song in selected_songs:
     )
     recommendations.append(recommendations_df)
 
-
-
+# Concatenate all recommendations into a single DataFrame
 all_recommendations_df = pd.concat(recommendations, ignore_index=True)
-# Sorts the recommendation by similarity score
+
+# Sort the recommendation by similarity score
 all_recommendations_df.sort_values(by='Similarity', ascending=False, inplace=True)
-# get the top 30 most similar recommendation
-top_30_recommendations_df = all_recommendations_df.head(30)
+
 all_recommendations_df.to_csv('recommendations.csv', index=False)
-    
-# Displays the recommendations in a dataframe.
-print(top_30_recommendations_df)
-    
+
+# Display the recommendations
+print(all_recommendations_df.head(30))
+
 # A plot for similarity score
 plt.figure(figsize=(10, 6))
-plt.bar(top_30_recommendations_df['Recommended Song'], top_30_recommendations_df['Similarity'], color='blue')
+plt.bar(all_recommendations_df['Recommended Song'], all_recommendations_df['Similarity'], color='blue')
 plt.xlabel('Recommended Song')
 plt.ylabel('Similarity Score')
-plt.title('Top 30 Recommended Songs and Their Similarity Scores')
+plt.title('Top Recommended Songs and Their Similarity Scores')
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 
